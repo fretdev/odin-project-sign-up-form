32f759e16c9a41db238c0de4e09759d7a7766142
Fretdev <ahmedibrahimprosper22@gmail.com>
2026-08-24 02:15:26 +0100

    feat:form project completed with confirm password validation

---
+++ index.js
@@ -0,0 +1,11 @@
+const password = document.getElementById('password')
+const confirmPassword = document.getElementById('confirm-password')
+
+confirmPassword.addEventListener('input',()=>{
+    if(confirmPassword.value != password.value){
+        confirmPassword.setCustomValidity('Passwords do not match')
+    }
+    else{
+        confirmPassword.setCustomValidity('')
+    }
+})
